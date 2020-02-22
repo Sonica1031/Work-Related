2bfb3c95328fb1410e8fed1a085e43a196c97645
Sonica1031 <53800242+Sonica1031@users.noreply.github.com>
2020-02-22 14:31:44 -0800

    Fixed bug

--- src/components/MenuButtonAdd.js
+++ src/components/MenuButtonAdd.js
@@ -1,17 +1,43 @@
 import React, { useState } from 'react';
-import MenuButton from './MenuButton';
 
 function MenuButtonAdd(){
+
 const [titleValue, setTitleValue] = useState("")
-const [showing, setShowing] = useState();
+const [showing, setShowing] = useState([]);
+let id = Date.now()
+
+    function MenuButton(){
+        const Handler = (e) => {
+          e.preventDefault()
+          let index = showing.findIndex(i => i.id === showing.id)
+                setShowing(showing.splice(index));
+            }
+      
+          return(
+      <form id={`.G${Date.now()}`}>
+      <p>{titleValue}</p>
+        <input type="radio" id={Date.now() + "-yes"} name={`${Date.now()}`} value="yes" required={true}/>
+        <label for="Menu-Item-1">Yes</label>
+        <input type="radio" id={Date.now() + "-no"} name={`${Date.now()}`} value="no"/>
+        <label for="Menu-Item-1">No</label>
+        <input type="radio" id={Date.now() + "sometimes"} name={`${Date.now()}`} value="sometimes"/>
+        <label for="Menu-Item-1">Sometimes</label>
+        <div>
+          <input type="text" placeholder="Any other comments" />
+        </div>
+        <button onClick={Handler}>Remove</button>
+      </form>
+          )
+      }
 
 const Handler = e =>{
     e.preventDefault();
     setTitleValue(e.target.value);
 }
 const submit = (e) => {
-e.preventDefault()
-setShowing ([showing, <MenuButton title={titleValue} group={`${Date.now()}`}/>])
+    e.preventDefault()
+    setShowing ([showing, MenuButton()])
+    localStorage.setItem('showing', showing);
 }
     return(
         <div>
